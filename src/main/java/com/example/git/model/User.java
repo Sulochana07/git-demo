04aa418528b97228499906716f7a777db1d2514e
Sulochana07 <sulochanasahu2002@gmail.com>
2025-02-15 16:15:16 +0530

    Modifying User -added new feild email

--- src/main/java/com/example/git/model/User.java
+++ src/main/java/com/example/git/model/User.java
@@ -29,4 +29,13 @@ public class User {
 
     private String username;
     private  String password;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
