8318afa45cfcea151c313cd5ba0086f912619fec
Sulochana07 <sulochanasahu2002@gmail.com>
2025-02-15 16:08:01 +0530

    Adding files

---
+++ src/main/java/com/example/git/model/User.java
@@ -0,0 +1,32 @@
+package com.example.git.model;
+
+public class User {
+    private int id;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String username;
+    private  String password;
+}
